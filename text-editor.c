611ff258bbfe3c355efdc0cb0f06a60bcef3fb08
Cyan <124202346+cyan-wolf@users.noreply.github.com>
2025-12-04 15:38:16 -0400

    add error handling

--- text-editor.c
+++ text-editor.c
@@ -1,19 +1,40 @@
+#include <asm-generic/errno-base.h>
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <errno.h>
+
+/*
+ * Data.
+ */
 
 // Caches the original terminal attributes for later cleanup.
 struct termios og_termios;
 
+/*
+ * Terminal handling.
+ */
+
+// Crash the program with an error message.
+void die(const char *message) {
+    perror(message);
+    exit(1);
+}
+
 void disableRawMode() {
-    tcsetattr(STDERR_FILENO, TCSAFLUSH, &og_termios);
+    if (tcsetattr(STDERR_FILENO, TCSAFLUSH, &og_termios) == -1) {
+        die("tcsetattr");
+    }
 }
 
 void enableTermRawMode() {
     // Get current terminal attribututes.
-    tcgetattr(STDIN_FILENO, &og_termios);
+    if (tcgetattr(STDIN_FILENO, &og_termios) == -1) {
+        die("tcsetattr");
+    }
     
     // Set a callback for disabling raw mode (cleeanup).
     atexit(disableRawMode);
@@ -21,8 +42,17 @@ void enableTermRawMode() {
     // Copy the current terminal attributes for futher modification.
     struct termios raw = og_termios;
 
-    // Diable the default CTRL-S and CTRL-Q handling.
-    raw.c_iflag &= ~(IXON);
+    // Disable the default CTRL-S and CTRL-Q handling.
+    // Disabling `ICRNL` makes CTRL-M be read as 13 (correct) instead of 10 (incorrect). 
+    raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
+
+    // Disable output processing (i.e. automatic carriage return insertion
+    // when the program itself prints a line).
+    raw.c_oflag &= ~(OPOST);
+
+    // Enable (if it isn't already) the bit mask for setting the character 
+    // size to 8 bits.
+    raw.c_cflag |= CS8;
 
     // Disable the:
     // - echo
@@ -30,22 +60,38 @@ void enableTermRawMode() {
     // - signal processing (i.e. from CTRL-C or CTRL-Z)
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
+    raw.c_cc[VMIN] = 0;     // minimum # of bytes before `read()` can return
+    raw.c_cc[VTIME] = 1;    // delay (in 10ths of a second) to wait before `read()` returns
+
     // Set the modified terminal attributes.
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
+        die("tcsetattr");
+    }
 }
 
+/*
+ * Program initialization.
+ */
 int main() {
     enableTermRawMode();
 
-    char c;
+    
+    while (true) {
+        char c;
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
+            die("read");
+        } 
 
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
         if (iscntrl(c)) { // when c is a control character (non-printable)
-            printf("%d\n", c);
+            printf("%d\r\n", c);
         }
         else {
             // Print both as the codepoint and the character.
-            printf("%d ('%c')\n", c, c);
+            printf("%d ('%c')\r\n", c, c);
+        }
+
+        if (c == 'q') {
+            break;
         }
     }
 
