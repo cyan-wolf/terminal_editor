da48b2baf095500072ef9a39f04bde970cc3d336
Cyan <124202346+cyan-wolf@users.noreply.github.com>
2025-12-03 21:03:26 -0400

    add character-by-character debug printing

--- text-editor.c
+++ text-editor.c
@@ -1,6 +1,8 @@
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
 
 // Caches the original terminal attributes for later cleanup.
 struct termios og_termios;
@@ -19,8 +21,14 @@ void enableTermRawMode() {
     // Copy the current terminal attributes for futher modification.
     struct termios raw = og_termios;
 
-    // Disable echo and canonical (line-by-line) mode.
-    raw.c_lflag &= ~(ECHO | ICANON);
+    // Diable the default CTRL-S and CTRL-Q handling.
+    raw.c_iflag &= ~(IXON);
+
+    // Disable the:
+    // - echo
+    // - canonical (line-by-line) mode.
+    // - signal processing (i.e. from CTRL-C or CTRL-Z)
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
     // Set the modified terminal attributes.
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
@@ -32,7 +40,13 @@ int main() {
     char c;
 
     while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
-
+        if (iscntrl(c)) { // when c is a control character (non-printable)
+            printf("%d\n", c);
+        }
+        else {
+            // Print both as the codepoint and the character.
+            printf("%d ('%c')\n", c, c);
+        }
     }
 
     return 0;
